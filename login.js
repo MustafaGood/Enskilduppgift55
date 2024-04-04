a399c0ba7dfba5e0678dfbd15e689d2d9df64b8e
Mustafa Salahuddin <97270548+MustafaGood@users.noreply.github.com>
2024-04-05 00:48:34 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault(); 
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    // Hämta värden från formuläret
     const username = document.getElementById('usernameLogin').value;
     const password = document.getElementById('passwordLogin').value;
 
     console.log('Loggar in användaren:', username);
+
+    localStorage.setItem('username', username);
+    window.location.href = 'profile.html';
 });
